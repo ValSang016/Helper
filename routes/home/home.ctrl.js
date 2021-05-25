ef5d710303a82e575d57102d58cd0a6e8fb5a565
unknown <tkdgjs1315@gc.gachon.ac.kr>
2021-05-26 01:42:49 +0900

    깃 저장소 초기화

---
+++ routes/home/home.ctrl.js
@@ -0,0 +1,14 @@
+"use strict";
+
+const home = (req, res) => {
+    res.render("home/index")
+};
+
+const login = (req, res) => {
+    res.render("home/index")
+};
+
+module.exports = {
+    home,
+    login,
+}
